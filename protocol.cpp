788e1c3303150fb16a742e15714c87eaf38ef1f8
Mahan <mahan.rafiei@yahoo.com>
2020-09-19 03:26:49 +0430

    Update protocol.cpp

--- protocol.cpp
+++ protocol.cpp
@@ -22,7 +22,7 @@ static const char* ppszTypeName[] =
     "block",
 };
 
-unsigned char pchMessageStart[4] = { 0xf9, 0xbe, 0xb4, 0xd9 };
+unsigned char pchMessageStart[4] = { 0xb1, 0xdc, 0x95, 0x85 };
 
 CMessageHeader::CMessageHeader()
 {
